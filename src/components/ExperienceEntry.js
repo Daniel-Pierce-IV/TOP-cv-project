a268111b6c1983db5e851ec38f80fde448be7575
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-08 14:07:22 -0500

    Implement highlight icons and fix deleter position

--- src/components/ExperienceEntry.js
+++ src/components/ExperienceEntry.js
@@ -74,9 +74,25 @@ class ExperienceEntry extends Entry {
                 className="relative mt-1 first:mt-0"
               >
                 <div
-                  className="delete-highlight absolute h-full aspect-square bg-red-300 -translate-x-full top-1/2 -translate-y-1/2 -left-1"
+                  className="delete-highlight absolute h-6 w-6 aspect-square text-red-400 bg-white -translate-x-full top-[12px] -translate-y-1/2 -left-1"
                   onClick={this.deleteHighlight.bind(this, index)}
-                ></div>
+                >
+                  <svg viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z"
+                    />
+                  </svg>
+                </div>
+
+                <div className="list-icon absolute h-4 w-4 -translate-x-full top-[12px] -translate-y-1/2 -left-0.5 ">
+                  <svg viewBox="2 2 20 20">
+                    <path
+                      fill="currentColor"
+                      d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
+                    />
+                  </svg>
+                </div>
 
                 <DynamicTextarea
                   className="w-full"
