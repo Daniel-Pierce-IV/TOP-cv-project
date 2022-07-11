3b2305f9157491c83089de28643f0b14f2b98c24
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-11 17:07:18 -0500

    Refactor DynamicTextarea as function component

--- src/components/DynamicTextarea.js
+++ src/components/DynamicTextarea.js
@@ -1,35 +1,29 @@
 import React from "react";
 
-class DynamicTextarea extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textareaRef = React.createRef();
-  }
+function DynamicTextarea(props) {
+  const { className, name, placeholder, value, onInput } = props;
+  const textareaRef = React.createRef();
 
   // Ensures that the textarea grows vertically with the text it contains
-  textareaResizer = (textareaRef) => {
+  const textareaResizer = (textareaRef) => {
     textareaRef.current.style.height = "auto";
     textareaRef.current.style.height = textareaRef.current.scrollHeight + "px";
   };
 
-  render() {
-    const { className, name, placeholder, value, onInput } = this.props;
-
-    return (
-      <textarea
-        ref={this.textareaRef}
-        className={`${className} block overflow-hidden resize-none`}
-        name={name}
-        placeholder={placeholder}
-        value={value ? value : ""}
-        rows="1"
-        onInput={(event) => {
-          onInput(event);
-          this.textareaResizer(this.textareaRef);
-        }}
-      ></textarea>
-    );
-  }
+  return (
+    <textarea
+      ref={textareaRef}
+      className={`${className} block overflow-hidden resize-none`}
+      name={name}
+      placeholder={placeholder}
+      value={value ? value : ""}
+      rows="1"
+      onInput={(event) => {
+        onInput(event);
+        textareaResizer(textareaRef);
+      }}
+    ></textarea>
+  );
 }
 
 export default DynamicTextarea;
