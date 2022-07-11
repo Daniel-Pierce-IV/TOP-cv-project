c444717fa6d813cbca2d59d653648b9bbb3d8748
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-11 17:43:35 -0500

    Refactor Section as function component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,33 +1,24 @@
-import React from "react";
 import "./Section.css";
 
-class Section extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function Section(props) {
+  const { children, createEntry, flexClasses, title } = props;
 
-  render() {
-    const { title, createEntry, children } = this.props;
+  return (
+    <section className="section relative">
+      <div className="flex justify-between border-b-2">
+        <h2 className="text-2xl">{title}</h2>
 
-    return (
-      <section className="section relative">
-        <div className="flex justify-between border-b-2">
-          <h2 className="text-2xl">{title}</h2>
+        <button
+          onClick={createEntry}
+          className="create-entry bg-gray-300 border-2 border-gray-600 self-center my-0.5 px-2 hover:brightness-90 active:brightness-[.8]"
+        >
+          Add Entry
+        </button>
+      </div>
 
-          <button
-            onClick={createEntry}
-            className="create-entry bg-gray-300 border-2 border-gray-600 self-center my-0.5 px-2 hover:brightness-90 active:brightness-[.8]"
-          >
-            Add Entry
-          </button>
-        </div>
-
-        <div className={`flex flex-col ${this.props.flexClasses}`}>
-          {children}
-        </div>
-      </section>
-    );
-  }
+      <div className={`flex flex-col ${flexClasses}`}>{children}</div>
+    </section>
+  );
 }
 
 export default Section;
