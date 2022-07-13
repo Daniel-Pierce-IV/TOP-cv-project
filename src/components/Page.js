bef3fa15e13b20c40b094668f34e01b260e10492
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-12 21:09:56 -0500

    Refactor Page as function component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,206 +1,136 @@
-import React from "react";
-import ExperienceEntry from "./ExperienceEntry";
-import ProjectEntry from "./ProjectEntry";
+import { createRef, useState, useEffect } from "react";
 import Header from "./Header";
 import Section from "./Section";
+import ExperienceFactory from "../Experience";
+import ExperienceEntry from "./ExperienceEntry";
+import SkillFactory from "../Skill";
 import SkillEntry from "./SkillEntry";
+import ProjectEntry from "./ProjectEntry";
+import ProjectFactory from "../Project";
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.pageElement = React.createRef();
+function Page() {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: null,
+    title: null,
+    phone: null,
+    email: null,
+    linkedin: null,
+    github: null,
+  });
 
-    this.state = {
-      personalInfo: {
-        fullName: null,
-        title: null,
-        phone: null,
-        email: null,
-        linkedin: null,
-        github: null,
-      },
-      entries: {
-        experience: [this.defaultExperience],
-        project: [this.defaultProject],
-        skill: [this.defaultSkill],
-      },
-    };
+  function updatePersonalInfo(changeObj) {
+    setPersonalInfo({
+      ...personalInfo,
+      ...changeObj,
+    });
   }
 
-  componentDidMount = () => {
-    // Maintains aspect ratio by setting max width to initial width
-    this.pageElement.current.style = `max-width: ${this.pageElement.current.clientWidth}px;`;
-  };
+  const [skills, setSkills] = useState(() => [SkillFactory()]);
 
-  updatePersonalInfo = (changeObj) => {
-    this.setState({
-      personalInfo: {
-        ...this.state.personalInfo,
-        ...changeObj,
-      },
-    });
-  };
-
-  get defaultExperience() {
-    return {
-      jobTitle: null,
-      employer: null,
-      timeframe: null,
-      highlights: [null],
-    };
+  function createSkill() {
+    setSkills(skills.concat(SkillFactory()));
   }
 
-  createExperience = () => {
-    this.updateEntries((curState) => {
-      curState.entries.experience.push(this.defaultExperience);
-    });
-  };
-
-  deleteExperience = (index) => {
-    this.updateEntries((curState) => {
-      curState.entries.experience = curState.entries.experience.filter(
-        (entry, entryIndex) => entryIndex !== index
-      );
-    });
-  };
-
-  updateExperience = (index, experienceState) => {
-    this.updateEntries((curState) => {
-      curState.entries.experience[index] = experienceState;
-    });
-  };
-
-  get defaultProject() {
-    return {
-      projectTitle: null,
-      url: null,
-      description: null,
-    };
+  function deleteSkill(index) {
+    setSkills(skills.filter((e, i) => i !== index));
   }
 
-  createProject = () => {
-    this.updateEntries((curState) => {
-      curState.entries.project.push(this.defaultProject);
-    });
-  };
-
-  deleteProject = (index) => {
-    this.updateEntries((curState) => {
-      curState.entries.project = curState.entries.project.filter(
-        (entry, entryIndex) => entryIndex !== index
-      );
-    });
-  };
-
-  updateProject = (index, projectState) => {
-    this.updateEntries((curState) => {
-      curState.entries.project[index] = projectState;
-    });
-  };
-
-  get defaultSkill() {
-    return {
-      text: null,
-    };
+  function updateSkill(index, changes) {
+    setSkills(skills.map((e, i) => (i !== index ? e : { ...e, ...changes })));
   }
 
-  createSkill = () => {
-    this.updateEntries((curState) => {
-      curState.entries.skill.push(this.defaultSkill);
-    });
-  };
+  const [experiences, setExperiences] = useState(() => [ExperienceFactory()]);
 
-  deleteSkill = (index) => {
-    this.updateEntries((curState) => {
-      curState.entries.skill = curState.entries.skill.filter(
-        (entry, entryIndex) => entryIndex !== index
-      );
-    });
-  };
+  function createExperience() {
+    setExperiences(experiences.concat(ExperienceFactory()));
+  }
 
-  updateSkill = (index, skillState) => {
-    this.updateEntries((curState) => {
-      curState.entries.skill[index] = skillState;
-    });
-  };
+  function deleteExperience(index) {
+    setExperiences(experiences.filter((e, i) => i !== index));
+  }
 
-  updateEntries = (mutatorFunction) => {
-    const stateClone = this.copyEntriesState();
-    mutatorFunction(stateClone);
-    this.setState(stateClone);
-  };
-
-  copyEntriesState = () => {
-    return {
-      entries: {
-        ...this.state.entries,
-        experience: this.state.entries.experience.map((entry) => ({
-          ...entry,
-          highlights: [...entry.highlights],
-        })),
-      },
-    };
-  };
-
-  render() {
-    return (
-      <div className="px">
-        <div
-          ref={this.pageElement}
-          className="page aspect-[8.5/11] h-full bg-white"
-        >
-          <Header
-            {...this.state.personalInfo}
-            updateInfo={this.updatePersonalInfo}
-          />
-
-          <div className="p-4 pt-0 flex flex-col gap-3">
-            <Section title="Skills" createEntry={this.createSkill}>
-              {this.state.entries.skill.map((entry, i) => (
-                <SkillEntry
-                  key={`skill-${i}`}
-                  entry={entry}
-                  updateEntry={this.updateSkill.bind(this, i)}
-                  deleteEntry={this.deleteSkill.bind(this, i)}
-                />
-              ))}
-            </Section>
-
-            <Section
-              title="Experience"
-              createEntry={this.createExperience}
-              flexClasses="gap-1.5"
-            >
-              {this.state.entries.experience.map((entry, i) => (
-                <ExperienceEntry
-                  key={`experience-${i}`}
-                  entryKey={i}
-                  entry={entry}
-                  updateEntry={this.updateExperience.bind(this, i)}
-                  deleteEntry={this.deleteExperience.bind(this, i)}
-                />
-              ))}
-            </Section>
-
-            <Section
-              title="Projects"
-              createEntry={this.createProject}
-              flexClasses="gap-1.5"
-            >
-              {this.state.entries.project.map((entry, i) => (
-                <ProjectEntry
-                  key={`project-${i}`}
-                  entry={entry}
-                  updateEntry={this.updateProject.bind(this, i)}
-                  deleteEntry={this.deleteProject.bind(this, i)}
-                />
-              ))}
-            </Section>
-          </div>
-        </div>
-      </div>
+  function updateExperience(index, changes) {
+    setExperiences(
+      experiences.map((e, i) => (i !== index ? e : { ...e, ...changes }))
     );
   }
+
+  const [projects, setProjects] = useState(() => [ProjectFactory()]);
+
+  function createProject() {
+    setProjects(projects.concat(ProjectFactory()));
+  }
+  function deleteProject(index) {
+    setProjects(projects.filter((e, i) => i !== index));
+  }
+
+  function updateProject(index, changes) {
+    setProjects(
+      projects.map((e, i) => (i !== index ? e : { ...e, ...changes }))
+    );
+  }
+
+  const pageElement = createRef();
+
+  useEffect(() => {
+    // Maintains aspect ratio by setting max width to initial width
+    pageElement.current.style = `max-width: ${pageElement.current.clientWidth}px;`;
+  }, []);
+
+  return (
+    <div className="px">
+      <div ref={pageElement} className="page aspect-[8.5/11] h-full bg-white">
+        <Header {...personalInfo} updateInfo={updatePersonalInfo} />
+
+        <div className="p-4 pt-0 flex flex-col gap-3">
+          <Section
+            title="Skill"
+            createEntry={createSkill}
+            flexClasses="gap-1.5"
+          >
+            {skills.map((skill, i) => (
+              <SkillEntry
+                key={skill.id}
+                updateEntry={(changes) => updateSkill(i, changes)}
+                deleteEntry={() => deleteSkill(i)}
+                {...skill}
+              />
+            ))}
+          </Section>
+
+          <Section
+            title="Experience"
+            createEntry={createExperience}
+            flexClasses="gap-1.5"
+          >
+            {experiences.map((experience, i) => (
+              <ExperienceEntry
+                key={experience.id}
+                updateEntry={(changes) => updateExperience(i, changes)}
+                deleteEntry={() => deleteExperience(i)}
+                {...experience}
+              />
+            ))}
+          </Section>
+
+          <Section
+            title="Project"
+            createEntry={createProject}
+            flexClasses="gap-1.5"
+          >
+            {projects.map((project, i) => (
+              <ProjectEntry
+                key={project.id}
+                updateEntry={(changes) => updateProject(i, changes)}
+                deleteEntry={() => deleteProject(i)}
+                {...project}
+              />
+            ))}
+          </Section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
