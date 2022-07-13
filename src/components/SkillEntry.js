2812d3dd7d7f43bbf39166fc76faa24342e15d1e
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-12 20:52:56 -0500

    Refactor SkillEntry as function component

--- src/components/SkillEntry.js
+++ src/components/SkillEntry.js
@@ -1,13 +1,8 @@
-import Entry from "./Entry";
-import React from "react";
+import ControlPanel from "./ControlPanel";
+import ControlButton from "./ControlButton";
 
-class SkillEntry extends Entry {
-  constructor(props) {
-    super(props);
-    this.inputKey = Entry.controlCount++;
-  }
-
-  processText = (text) => {
+export default function SkillEntry(props) {
+  function processText(text) {
     let label,
       skills,
       usingPlaceholder = true;
@@ -25,47 +20,47 @@ class SkillEntry extends Entry {
     }
 
     return { label, skills, usingPlaceholder };
-  };
+  }
 
-  render() {
-    const labelPlaceholder = "Bold Label (if colon)->:";
-    const skillsPlaceholder = " HTML, CSS, JS";
-    const { text } = this.props.entry;
-    const { label, skills, usingPlaceholder } = this.processText(text);
+  function updateProperty(propertyName, event) {
+    props.updateEntry({ [propertyName]: event.target.value });
+  }
 
-    const controlOverlay = this.renderControlOverlay(
-      this.createControl("Delete Entry", this.deleteEntry, "bg-red-300"),
-      <input
-        type="text"
-        key={this.inputKey}
-        className="grow"
-        size={40}
-        placeholder="Enter your skills here"
-        value={text ? text : ""}
-        onChange={this.updateValue.bind(this, "text")}
-      />
-    );
+  const labelPlaceholder = "Bold Label (if colon)->:";
+  const skillsPlaceholder = " HTML, CSS, JS";
+  const { text, deleteEntry } = props;
+  const { label, skills, usingPlaceholder } = processText(text);
 
-    return (
-      <div className="skill entry relative">
-        {controlOverlay}
+  return (
+    <div className="skill entry relative">
+      <ControlPanel>
+        <ControlButton
+          label="Delete Entry"
+          onClick={deleteEntry}
+          colorClass="bg-red-300"
+        />
+        <input
+          type="text"
+          className="grow"
+          size={40}
+          placeholder="Enter your skills here"
+          value={text ? text : ""}
+          onChange={updateProperty.bind(null, "text")}
+        />
+      </ControlPanel>
 
-        <div className="w-full relative">
-          <div
-            ref={this.skillsRef}
-            className={`w-full cursor-default ${
-              usingPlaceholder ? "text-gray-400" : ""
-            }`}
-          >
-            <span className="font-bold">
-              {usingPlaceholder ? labelPlaceholder : label}
-            </span>
-            {usingPlaceholder ? skillsPlaceholder : skills}
-          </div>
+      <div className="w-full relative">
+        <div
+          className={`w-full cursor-default ${
+            usingPlaceholder ? "text-gray-400" : ""
+          }`}
+        >
+          <span className="font-bold">
+            {usingPlaceholder ? labelPlaceholder : label}
+          </span>
+          {usingPlaceholder ? skillsPlaceholder : skills}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default SkillEntry;
