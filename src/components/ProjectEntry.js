9c432964d48e09ff81a73d4fe7340f42298ad673
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-12 21:07:07 -0500

    Refactor ProjectEntry as function component

--- src/components/ProjectEntry.js
+++ src/components/ProjectEntry.js
@@ -1,53 +1,54 @@
-import Entry from "./Entry";
+import ControlButton from "./ControlButton";
+import ControlPanel from "./ControlPanel";
 import DynamicTextarea from "./DynamicTextarea";
 
-class ProjectEntry extends Entry {
-  constructor(props) {
-    super(props);
+export default function ProjectEntry(props) {
+  const { projectTitle, url, description, deleteEntry, updateEntry } = props;
+  const mainClasses = "basis-0 grow whitespace-nowrap ";
+
+  function updateProperty(propertyName, event) {
+    updateEntry({ [propertyName]: event.target.value });
   }
 
-  render() {
-    const { projectTitle, url, description } = this.props.entry;
-    const mainClasses = "basis-0 grow whitespace-nowrap ";
+  return (
+    <div className="experience entry relative">
+      <ControlPanel>
+        <ControlButton
+          label="Delete Entry"
+          onClick={deleteEntry}
+          colorClass="bg-red-300"
+        />
+      </ControlPanel>
 
-    return (
-      <div className="experience entry relative">
-        {this.renderControlOverlay(
-          this.createControl("Delete Entry", this.deleteEntry, "bg-red-300")
-        )}
+      <div className="flex justify-between mb-[1px]">
+        <input
+          type="text"
+          className={mainClasses + " mr-[1px]"}
+          name="projectTitle"
+          placeholder="Project Title"
+          value={projectTitle ? projectTitle : ""}
+          onInput={updateProperty.bind(null, "projectTitle")}
+        />
 
-        <div className="flex justify-between mb-[1px]">
-          <input
-            type="text"
-            className={mainClasses + " mr-[1px]"}
-            name="projectTitle"
-            placeholder="Project Title"
-            value={projectTitle ? projectTitle : ""}
-            onInput={this.updateValue.bind(this, "projectTitle")}
-          />
-
-          <input
-            type="text"
-            className={mainClasses + "text-right"}
-            name="url"
-            placeholder="https://www.example.com"
-            value={url ? url : ""}
-            onInput={this.updateValue.bind(this, "url")}
-          />
-        </div>
-
-        <div className="flex">
-          <DynamicTextarea
-            className="grow ml-4"
-            name={description}
-            placeholder="Description of your project"
-            value={description}
-            onInput={this.updateValue.bind(this, "description")}
-          />
-        </div>
+        <input
+          type="text"
+          className={mainClasses + "text-right"}
+          name="url"
+          placeholder="https://www.example.com"
+          value={url ? url : ""}
+          onInput={updateProperty.bind(null, "url")}
+        />
       </div>
-    );
-  }
-}
 
-export default ProjectEntry;
+      <div className="flex">
+        <DynamicTextarea
+          className="grow ml-4"
+          name={description}
+          placeholder="Description of your project"
+          value={description}
+          onInput={updateProperty.bind(null, "description")}
+        />
+      </div>
+    </div>
+  );
+}
